ee9b82c46fdf039168b645faca7da1a2a53a74e6
John McClean <john.mcclean@teamaol.com>
2016-11-06 21:36:01 +0000

    unit tests and javadoc

--- cyclops-typeclasses/src/main/java/com/aol/cyclops/hkt/instances/jdk/Lists.java
+++ cyclops-typeclasses/src/main/java/com/aol/cyclops/hkt/instances/jdk/Lists.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.aol.cyclops.Monoid;
 import com.aol.cyclops.data.collections.extensions.standard.ListX;
@@ -43,36 +45,178 @@ public class Lists {
                     
         System.out.println(Lists.functor().map(i->i*2, ListType.widen(small)));
     }
+    /**
+     * 
+     * Transform a list, mulitplying every element by 2
+     * 
+     * <pre>
+     * {@code 
+     *  ListType<Integer> list = Lists.functor().map(i->i*2, ListType.widen(Arrays.asList(1,2,3));
+     *  
+     *  //[2,4,6]
+     *  
+     * 
+     * }
+     * </pre>
+     * 
+     * An example fluent api working with Lists
+     * <pre>
+     * {@code 
+     *   ListType<Integer> list = Lists.unit()
+                                       .unit("hello")
+                                       .then(h->Lists.functor().map((String v) ->v.length(), h))
+                                       .convert(ListType::narrowK);
+     * 
+     * }
+     * </pre>
+     * 
+     * 
+     * @return A functor for Lists
+     */
     public static <T,R>Functor<ListType.µ> functor(){
         BiFunction<ListType<T>,Function<? super T, ? extends R>,ListType<R>> map = Lists::map;
         return General.functor(map);
     }
+    /**
+     * <pre>
+     * {@code 
+     * ListType<String> list = Lists.unit()
+                                     .unit("hello")
+                                     .convert(ListType::narrowK);
+        
+        //Arrays.asList("hello"))
+     * 
+     * }
+     * </pre>
+     * 
+     * 
+     * @return A factory for Lists
+     */
     public static Unit<ListType.µ> unit(){
         return General.unit(Lists::of);
     }
+    /**
+     * 
+     * <pre>
+     * {@code 
+     * import static com.aol.cyclops.hkt.jdk.ListType.widen;
+     * import static com.aol.cyclops.util.function.Lambda.l1;
+     * import static java.util.Arrays.asList;
+     * 
+       Lists.zippingApplicative()
+            .ap(widen(asList(l1(this::multiplyByTwo))),widen(asList(1,2,3)));
+     * 
+     * //[2,4,6]
+     * }
+     * </pre>
+     * 
+     * 
+     * Example fluent API
+     * <pre>
+     * {@code 
+     * ListType<Function<Integer,Integer>> listFn =Lists.unit()
+     *                                                  .unit(Lambda.l1((Integer i) ->i*2))
+     *                                                  .convert(ListType::narrowK);
+        
+        ListType<Integer> list = Lists.unit()
+                                      .unit("hello")
+                                      .then(h->Lists.functor().map((String v) ->v.length(), h))
+                                      .then(h->Lists.zippingApplicative().ap(listFn, h))
+                                      .convert(ListType::narrowK);
+        
+        //Arrays.asList("hello".length()*2))
+     * 
+     * }
+     * </pre>
+     * 
+     * 
+     * @return A zipper for Lists
+     */
     public static <T,R> Applicative<ListType.µ> zippingApplicative(){
         BiFunction<ListType< Function<T, R>>,ListType<T>,ListType<R>> ap = Lists::ap;
         return General.applicative(functor(), unit(), ap);
     }
+    /**
+     * 
+     * <pre>
+     * {@code 
+     * import static com.aol.cyclops.hkt.jdk.ListType.widen;
+     * ListType<Integer> list  = Lists.monad()
+                                      .flatMap(i->widen(ListX.range(0,i)), widen(Arrays.asList(1,2,3)))
+                                      .convert(ListType::narrowK);
+     * }
+     * </pre>
+     * 
+     * Example fluent API
+     * <pre>
+     * {@code 
+     *    ListType<Integer> list = Lists.unit()
+                                        .unit("hello")
+                                        .then(h->Lists.monad().flatMap((String v) ->Lists.unit().unit(v.length()), h))
+                                        .convert(ListType::narrowK);
+        
+        //Arrays.asList("hello".length())
+     * 
+     * }
+     * </pre>
+     * 
+     * @return Type class with monad functions for Lists
+     */
     public static <T,R> Monad<ListType.µ> monad(){
   
         BiFunction<Higher<ListType.µ,T>,Function<? super T, ? extends Higher<ListType.µ,R>>,Higher<ListType.µ,R>> flatMap = Lists::flatMap;
         return General.monad(zippingApplicative(), flatMap);
     }
+    /**
+     * 
+     * <pre>
+     * {@code 
+     *  ListType<String> list = Lists.unit()
+                                     .unit("hello")
+                                     .then(h->Lists.monadZero().filter((String t)->t.startsWith("he"), h))
+                                     .convert(ListType::narrowK);
+        
+       //Arrays.asList("hello"));
+     * 
+     * }
+     * </pre>
+     * 
+     * 
+     * @return A filterable monad (with default value)
+     */
     public static <T,R> MonadZero<ListType.µ> monadZero(){
         
         return General.monadZero(monad(), ListType.widen(new ArrayList<T>()));
     }
+    /**
+     * <pre>
+     * {@code 
+     *  ListType<Integer> list = Lists.<Integer>monadPlus()
+                                      .plus(ListType.widen(Arrays.asList()), ListType.widen(Arrays.asList(10)))
+                                      .convert(ListType::narrowK);
+        //Arrays.asList(10))
+     * 
+     * }
+     * </pre>
+     * @return Type class for combining Lists by concatenation
+     */
     public static <T> MonadPlus<ListType.µ,T> monadPlus(){
-        Monoid<ListType<T>> m = Monoid.of(ListType.widen(new ArrayList<T>()), (l1,l2)->{ l1.addAll(l2); return l1;});
+        Monoid<ListType<T>> m = Monoid.of(ListType.widen(new ArrayList<T>()), Lists::concat);
         Monoid<Higher<ListType.µ,T>> m2= (Monoid)m;
         return General.monadPlus(monadZero(),m2);
     }
+    /**
+     * @param m Monoid to use for combining Lists
+     * @return Type class for combining Lists
+     */
     public static <T> MonadPlus<ListType.µ,T> monadPlus(Monoid<ListType<T>> m){
         Monoid<Higher<ListType.µ,T>> m2= (Monoid)m;
         return General.monadPlus(monadZero(),m2);
     }
  
+    /**
+     * @return Type class for traversables with traverse / sequence operations
+     */
     public static <C2,T> Traverse<ListType.µ> traverse(){
         BiFunction<Applicative<C2>,ListType<Higher<C2, T>>,Higher<C2, ListType<T>>> sequenceFn = (ap,list) -> {
         
@@ -94,12 +238,18 @@ public class Lists {
         return General.traverse(zippingApplicative(), sequenceNarrow);
     }
     
-    public static <T> Foldable<ListType.µ> folable(){
+    /**
+     * @return Type class for folding / reduction operations
+     */
+    public static <T> Foldable<ListType.µ> foldable(){
         BiFunction<Monoid<T>,Higher<ListType.µ,T>,T> foldRightFn =  (m,l)-> ListX.fromIterable(ListType.narrow(l)).foldRight(m);
         BiFunction<Monoid<T>,Higher<ListType.µ,T>,T> foldLeftFn = (m,l)-> ListX.fromIterable(ListType.narrow(l)).reduce(m);
         return General.foldable(foldRightFn, foldLeftFn);
     }
   
+    private static  <T> ListType<T> concat(List<T> l1, List<T> l2){
+        return ListType.widen(Stream.concat(l1.stream(),l2.stream()).collect(Collectors.toList()));
+    }
     private <T> ListType<T> of(T value){
         return ListType.widen(Arrays.asList(value));
     }
--- cyclops-typeclasses/src/test/java/com/aol/cyclops/hkt/instances/jdk/ListsTest.java
+++ cyclops-typeclasses/src/test/java/com/aol/cyclops/hkt/instances/jdk/ListsTest.java
@@ -1,5 +1,7 @@
 package com.aol.cyclops.hkt.instances.jdk;
-
+import static com.aol.cyclops.hkt.jdk.ListType.widen;
+import static com.aol.cyclops.util.function.Lambda.l1;
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -8,6 +10,7 @@ import java.util.function.Function;
 
 import org.junit.Test;
 
+import com.aol.cyclops.data.collections.extensions.standard.ListX;
 import com.aol.cyclops.hkt.jdk.ListType;
 import com.aol.cyclops.util.function.Lambda;
 
@@ -33,6 +36,14 @@ public class ListsTest {
         assertThat(list,equalTo(Arrays.asList("hello".length())));
     }
     @Test
+    public void apSimple(){
+        Lists.zippingApplicative()
+            .ap(widen(asList(l1(this::multiplyByTwo))),widen(asList(1,2,3)));
+    }
+    private int multiplyByTwo(int x){
+        return x*2;
+    }
+    @Test
     public void applicative(){
         
         ListType<Function<Integer,Integer>> listFn =Lists.unit().unit(Lambda.l1((Integer i) ->i*2)).convert(ListType::narrowK);
@@ -44,6 +55,12 @@ public class ListsTest {
                                      .convert(ListType::narrowK);
         
         assertThat(list,equalTo(Arrays.asList("hello".length()*2)));
+    }
+    @Test
+    public void monadSimple(){
+       ListType<Integer> list  = Lists.monad()
+                                      .flatMap(i->widen(ListX.range(0,i)), widen(Arrays.asList(1,2,3)))
+                                      .convert(ListType::narrowK);
     }
     @Test
     public void monad(){
@@ -74,6 +91,28 @@ public class ListsTest {
                                      .convert(ListType::narrowK);
         
         assertThat(list,equalTo(Arrays.asList()));
+    }
+    
+    @Test
+    public void monadPlus(){
+        ListType<Integer> list = Lists.<Integer>monadPlus()
+                                      .plus(ListType.widen(Arrays.asList()), ListType.widen(Arrays.asList(10)))
+                                      .convert(ListType::narrowK);
+        assertThat(list,equalTo(Arrays.asList(10)));
+    }
+    @Test
+    public void  foldLeft(){
+        int sum  = Lists.foldable()
+                        .foldLeft(0, (a,b)->a+b, ListType.widen(Arrays.asList(1,2,3,4)));
+        
+        assertThat(sum,equalTo(10));
+    }
+    @Test
+    public void  foldRight(){
+        int sum  = Lists.foldable()
+                        .foldRight(0, (a,b)->a+b, ListType.widen(Arrays.asList(1,2,3,4)));
+        
+        assertThat(sum,equalTo(10));
     }
     
 }
