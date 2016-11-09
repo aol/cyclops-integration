e3829d204e78511a4e8f075b2cbb5b4c2aa64ca5
John McClean <john.mcclean@teamaol.com>
2016-11-09 00:26:32 +0000

    derive4j hkt deps commented

--- cyclops-higherkindedtypes/src/main/java/com/aol/cyclops/hkt/alias/Higher2.java
+++ cyclops-higherkindedtypes/src/main/java/com/aol/cyclops/hkt/alias/Higher2.java
@@ -1,5 +1,7 @@
 package com.aol.cyclops.hkt.alias;
 
+import org.derive4j.hkt.__2;
+
 /**
  * 
  * Higher Kinded Type - a core type (e.g. a Xor) and the data types which it may store / manipulate (e.g. String and Exception).
@@ -12,7 +14,7 @@ package com.aol.cyclops.hkt.alias;
  * @param <T2> First data type of the Core Type
  * @param <T3> Second data type of the Core type
  */
-public interface Higher2<T1,T2,T3> extends Convert<Higher2<T1,T2,T3>>{//,__2<T1,T2, T3> {
+public interface Higher2<T1,T2,T3> extends Convert<Higher2<T1,T2,T3>> {//,__2<T1,T2, T3> {
     
    
 }
