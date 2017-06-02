b13ff91f16e0ef0c84de6d794ff201d8d18aa34a
John McClean <john.mcclean@teamaol.com>
2017-06-02 14:05:59 +0100

    ObservableT

--- cyclops-reactor/src/main/java/cyclops/monads/ReactorWitness.java
+++ cyclops-reactor/src/main/java/cyclops/monads/ReactorWitness.java
@@ -4,8 +4,16 @@ package cyclops.monads;
 import com.aol.cyclops.reactor.adapter.FluxAdapter;
 import com.aol.cyclops.reactor.adapter.MonoAdapter;
 import com.aol.cyclops2.types.extensability.FunctionalAdapter;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ReactorWitness {
+    public static <T> Flux<T> flux(AnyM<flux,? extends T> anyM){
+        return anyM.unwrap();
+    }
+    public static <T> Mono<T> mono(AnyM<mono,? extends T> anyM){
+        return anyM.unwrap();
+    }
     static interface FluxWitness<W extends ReactorWitness.FluxWitness<W>>  extends WitnessType<W> {
 
     }
--- cyclops-rx/src/main/java/cyclops/monads/RxWitness.java
+++ cyclops-rx/src/main/java/cyclops/monads/RxWitness.java
@@ -4,8 +4,15 @@ package cyclops.monads;
 
 import com.aol.cyclops.rx.adapter.ObservableAdapter;
 import com.aol.cyclops2.types.extensability.FunctionalAdapter;
+import rx.Observable;
+
 
 public interface RxWitness {
+
+    public static <T> Observable<T> observable(AnyM<obsvervable,? extends T> anyM){
+        return anyM.unwrap();
+    }
+
     static interface ObservableWitness<W extends RxWitness.ObservableWitness<W>>  extends WitnessType<W> {
 
     }
